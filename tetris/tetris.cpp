996400afa6049e2cc5d3d7837ddbebe10f6f6eb3
N00rAhmed <noor.ahmed232003@outlook.com>
2026-04-10 19:22:05 +0100

    removed commented code

--- tetris/tetris.cpp
+++ tetris/tetris.cpp
@@ -10,16 +10,13 @@
 #include <thread>
 #include <cstdio>
 
-//#include <ctime>
-//#include <chrono>
-
 
     // make a grid - DONE
     // make square bracket blocks controls(up down left right) in grid - DONE
     // animate blocks moving from up to down in grid - DONE
     // after one block placed generate new block coming from top - DONE
     // make blocks stack and place in a row inside grid - 
-    // if row full of blocks then remove them - 
+    // if row full of blocks then remove them - DONE
 
 
 using namespace std;
@@ -50,11 +47,17 @@ void display_grid(string _grid[4][3], int r, int c) {
 
 
     for (int row = 0; row < 4; row++) {
+
+        //cout << "#";
+
         for (int column = 0; column < 3; column++) {
             //grid[0][1] = "[]";
+            //cout << "#";
             cout << "\t" << grid[row][column];
         }
         cout << "\n" << "\n";
+        //cout << "\t" << "\t";
+
     }
 
 };
@@ -71,19 +74,8 @@ int main()
     }
     
     string key;
-    
     int row = 0;
     int column = 1;
-    
-
-
-    //grid[row][column] = "[]";
-    //display_grid(grid);
-
-    // arguments here setting the location of []
-    //display_grid(grid, row, column);
-
-
     int speed = 20;
     int speedcount = 0;
 
@@ -134,11 +126,6 @@ int main()
 
             }
 
-            //printf("hello");
-            //std::this_thread::sleep_for(500ms);
-            //printf("\rbye");
-            //std::this_thread::sleep_for(500ms);
-
 
             if (speedcount == speed) {
 
@@ -146,10 +133,17 @@ int main()
                     //grid[row + 3][column + 0] = "-";
                     //grid[row + 3][column + 1] = "-";
                     //grid[row + 3][column + 2] = "-";
-                    grid[3][0] = "-";
-                    grid[3][1] = "-";
-                    grid[3][2] = "-";
+                    grid[3][0] = ".";
+                    grid[3][1] = ".";
+                    grid[3][2] = ".";
                 }
+
+
+                //if (grid[3][0] == "[]") {
+                //    //grid[row-1][column] = "[]";
+                //    //row++;
+                //    grid[2][0] = "[]";
+                //}
 
 
                 speedcount = 0;
@@ -158,87 +152,19 @@ int main()
                 if (row + 1 < 4) {
                     row++;
                     grid[row][column] = "[]";
-
-                    //std::this_thread::sleep_for(500ms);
-                    //grid[row][column] = "\r-";
-
-                    //std::this_thread::sleep_for(100ms);
-                    //system("CLS");
-                    //cout << (grid[row][column] = "-");
-                    //std::this_thread::sleep_for(100ms);
-                    //system("CLS");
-                    //cout << (grid[row][column] = "x");
-
-
-                    //std::this_thread::sleep_for(100ms);
-                    //grid[row][column] = "-";
                 }
-
-                //if (grid[3][0] == "[]" and grid[3][1] == "[]" and grid[3][2] == "[]") {
-                //    //grid[row + 3][column + 0] = "-";
-                //    //grid[row + 3][column + 1] = "-";
-                //    //grid[row + 3][column + 2] = "-";
-                //    grid[3][0] = "-";
-                //    grid[3][1] = "-";
-                //    grid[3][2] = "-";
-                //}
-
                 
-                //auto timee = std::this_thread::sleep_for(100ms);
-
-                //auto start = std::chrono::steady_clock::now();
-                //std::this_thread::sleep_for(std::chrono::milliseconds(500));
-                //auto end = std::chrono::steady_clock::now();
-                //auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-                //duration.count();
-
-                //if (row == 3 && duration) {
-
-                //}
-
                 if (row == 3) {
-                    //cout << "row index: " << row << "\n" << "column index: " << column;
-                    //std::this_thread::sleep_for(500ms);
-                    //this_thread::sleep_for(chrono::milliseconds(5000));
-
                     row = 0;
                     column = 1;
                     grid[row][column] = "[]"; // brings [] to the top
                 }
 
-                //if (grid[row=3][column=1] == "[]") {
-                //    grid[row=3][column=1] = "-";
-                //}
 
             }
 
-
-
-            //if (grid[row + 3][column + 0] == "[]") {
-            //    grid[row + 3][column + 0] = "-";
-            //}
-
-
-            //if (grid[row + 3][column + 0] == "[]") {
-                //if (row + 1 < 4) {
-                    //swap(grid[row][column], grid[row + 1][column]);
-                    //grid[row + 3][column + 0] = "-";
-                    //row++;
-                //}
-            //}
-
-            //if (grid[row + 3][column + 0] == "[]" and grid[row + 3][column + 1] == "[]" and grid[row + 3][column + 2] == "[]") {
-            //    grid[row + 3][column + 0] = "-";
-            //    grid[row + 3][column + 1] = "-";
-            //    grid[row + 3][column + 2] = "-";
-            //}
-
-
-
             system("CLS");
-            //display_grid(grid);
             display_grid(grid, row, column);
-            //display_grid(grid);
     }
 };
 
